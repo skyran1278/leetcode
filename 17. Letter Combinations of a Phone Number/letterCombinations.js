1aa604aab2ab4ae8dde87ba0b3d37cbe1d49a145
skyran1278 <skyran1278@gmail.com>
2020-04-12 19:15:11 +0800

    17

---
+++ 17. Letter Combinations of a Phone Number/letterCombinations.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = (digits) => {
+  const map = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+  };
+};
