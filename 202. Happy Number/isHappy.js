7edd4a29549074c0fcede74954ff84f5bbc9a986
skyran1278 <skyran1278@gmail.com>
2020-04-14 21:46:27 +0800

    202

---
+++ 202. Happy Number/isHappy.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isHappy = (n) => {
+  const squares = {
+    0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81,
+  };
+  const seen = {};
+  const happy = (n) => {
+    if (n === 1) return true;
+    if (seen[n]) return false;
+    seen[n] = true;
+    const s = n.toString(10);
+    return happy(s.split('').reduce((sum, v) => sum + squares[v], 0));
+  };
+
+  return happy(n);
+};
+
+console.log(isHappy(19));
