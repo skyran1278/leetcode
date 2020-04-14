fe2fea3c0637f578a701a432ef2f5ff097227dbb
skyran1278 <skyran1278@gmail.com>
2020-04-14 21:50:06 +0800

    202

--- 202. Happy Number/isHappy.js
+++ 202. Happy Number/isHappy.js
@@ -7,15 +7,23 @@ const isHappy = (n) => {
     0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81,
   };
   const seen = {};
-  const happy = (n) => {
-    if (n === 1) return true;
-    if (seen[n]) return false;
-    seen[n] = true;
-    const s = n.toString(10);
-    return happy(s.split('').reduce((sum, v) => sum + squares[v], 0));
-  };
+  let happy = n;
+  while (!seen[happy]) {
+    seen[happy] = true;
+    const s = happy.toString(10);
+    happy = s.split('').reduce((sum, v) => sum + squares[v], 0);
+    if (happy === 1) return true;
+  }
+  return false;
+  // const happy = (n) => {
+  //   if (n === 1) return true;
+  //   if (seen[n]) return false;
+  //   seen[n] = true;
+  //   const s = n.toString(10);
+  //   return happy(s.split('').reduce((sum, v) => sum + squares[v], 0));
+  // };
 
-  return happy(n);
+  // return happy(n);
 };
 
 console.log(isHappy(19));
