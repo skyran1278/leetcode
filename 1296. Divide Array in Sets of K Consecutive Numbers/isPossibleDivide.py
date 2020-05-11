6330bad48f4987d601f71e442f7016dd8ef10a6b
skyran1278 <skyran1278@gmail.com>
2020-05-11 23:12:31 +0800

    python

--- 1296. Divide Array in Sets of K Consecutive Numbers/isPossibleDivide.py
+++ 1296. Divide Array in Sets of K Consecutive Numbers/isPossibleDivide.py
@@ -1,17 +1,26 @@
+from collections import Counter
+from typing import List
+
+
 class Solution:
-  def isPossibleDivide(self, nums: List[int], k: int) -> bool:
-    if len(nums) % k != 0:
-      return False
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        if len(nums) % k != 0:
+            return False
 
-    nums.sort()
-    nums_dict = Counter(nums)
+        nums.sort()
+        nums_dict = Counter(nums)
 
-    for num in nums_dict:
-      value = nums_dict[num]
-      if value == 0:
-        continue
-      for i in range(k):
-        if nums_dict[num + i] == 0:
-          return False
-        nums_dict[num + i] -= value
-    return True
+        for num in nums_dict:
+            value = nums_dict[num]
+            if value == 0:
+                continue
+            for i in range(k):
+                if nums_dict[num + i] == 0:
+                    return False
+                nums_dict[num + i] -= value
+        return True
+
+
+s = Solution()
+
+print(s.isPossibleDivide([1, 2, 3, 3, 4, 4, 5, 6], 4))
