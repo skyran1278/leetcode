be5fc0ecf48878550b294d361bba91e0dec2f53e
skyran1278 <skyran1278@gmail.com>
2020-05-22 12:45:12 +0800

    322

--- 322. Coin Change/coinChange.js
+++ 322. Coin Change/coinChange.js
@@ -4,6 +4,9 @@
  * @return {number}
  */
 const coinChange = (coins, amount) => {
+  // 加速
+  coins.sort((a, b) => b - a);
+
   const memo = Array(amount + 1);
   const dp = (n) => {
     // base case
@@ -28,38 +31,21 @@ const coinChange = (coins, amount) => {
 };
 
 const coinChange = (coins, amount) => {
-  const dp = Array(amount + 1);
+
+  coins.sort((a, b) => b - a);
+  // 初始化
+  const dp = Array(amount + 1).fill(Infinity);
   dp[0] = 0;
+
   for (let index = 0; index < dp.length; index += 1) {
-    const element = dp[index];
     coins.forEach((coin) => {
-      const subProblem = dp(n - coin);
+      const subProblem = dp[index - coin];
       if (subProblem >= 0) {
-        num = Math.min(num, subProblem + 1);
+        dp[index] = Math.min(dp[index], subProblem + 1);
       }
     });
   }
-  return dp[amount]
-  const dp = (n) => {
-    // base case
-    if (n === 0) return 0;
-    if (n < 0) return -1;
-
-    if (memo[n]) return memo[n];
-
-    let num = Infinity;
-    coins.forEach((coin) => {
-      const subProblem = dp(n - coin);
-      if (subProblem >= 0) {
-        num = Math.min(num, subProblem + 1);
-      }
-    });
-
-    memo[n] = Number.isFinite(num) ? num : -1;
-
-    return memo[n];
-  };
-  return dp(amount);
+  return Number.isFinite(dp[amount]) ? dp[amount] : -1;
 };
 
 console.log(coinChange([1, 2, 5], 11));
