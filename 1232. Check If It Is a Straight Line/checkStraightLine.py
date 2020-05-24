80f19b087c563ab6ce1c24841f5f2c17a07c07af
skyran1278 <skyran1278@gmail.com>
2020-05-24 08:12:38 +0800

    fix error

--- 1232. Check If It Is a Straight Line/checkStraightLine.py
+++ 1232. Check If It Is a Straight Line/checkStraightLine.py
@@ -1,20 +1,21 @@
+from typing import List
+
+
 class Solution:
-  def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-    (x1, y1) = coordinates[0]
-    (x2, y2) = coordinates[1]
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        (x1, y1) = coordinates[0]
+        (x2, y2) = coordinates[1]
 
-    # y = mx + b
-    if x2 - x1 != 0:
-      m = (y2 - y1) / (x2 - x1)
-      b = y1 - m * x1
-      for (x, y) in coordinates:
-        if m * x + b != y:
-          return False
-    else:
-      m = x1
-      for (x, y) in coordinates:
-        if x != m:
-          return False
-
-
-    return True
+        # y = mx + b
+        if x2 - x1 != 0:
+            m = (y2 - y1) / (x2 - x1)
+            b = y1 - m * x1
+            for (x, y) in coordinates:
+                if m * x + b != y:
+                    return False
+        else:
+            m = x1
+            for (x, y) in coordinates:
+                if x != m:
+                    return False
+        return True
