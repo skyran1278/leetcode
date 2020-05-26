5a60dbeb6d0d39d4c10ae7e38a7273522a376162
skyran1278 <skyran1278@gmail.com>
2020-05-26 22:43:44 +0800

    排列組合

---
+++ 46. Permutations/permute.js
@@ -0,0 +1,33 @@
+/**
+* @param {number[]} nums
+* @return {number[][]}
+*/
+const permute = (nums) => {
+  // 最終輸出的排列組合
+  const permutations = [];
+
+  const backtrack = (selects, rest) => {
+    const { length } = rest;
+    if (length === 0) {
+      permutations.push([...selects]);
+      return;
+    }
+
+    for (let i = 0; i < length; i += 1) {
+      // 做選擇
+      selects.push(rest.splice(i, 1)[0]);
+
+      // 往下遍歷
+      backtrack(selects, rest);
+
+      // 回朔選擇
+      rest.splice(i, 0, selects.pop());
+    }
+  };
+
+  backtrack([], nums);
+
+  return permutations;
+};
+
+console.log(permute([1, 2, 3]));
