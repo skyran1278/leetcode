e4a78b3836ea65b440230d6829bf7907ab6eb649
ran <skyran1278@gmail.com>
2021-04-20 22:34:28 +0800

    last error

--- 10. Regular Expression Matching/isMatch.js
+++ 10. Regular Expression Matching/isMatch.js
@@ -5,12 +5,25 @@
  */
 const isMatch = (s, p) => {
   if (s === p) return true;
-  const firstMatch = p[0] in [s[0], '.'];
+
+  const firstMatch = s[0] !== undefined && (p[0] === s[0] || p[0] === '.');
+
   if (p.length >= 2 && p[1] === '*') {
-    return firstMatch ? isMatch(s.substring(1, s.length), p) : isMatch(s, p(2, p.length));
+    return firstMatch
+      ? isMatch(s.substring(1, s.length), p)
+      : isMatch(s, p.substring(2, p.length));
   }
-  return firstMatch && isMatch(s.substring(1, s.length), p.substring(1, p.length));
+
+  return (
+    firstMatch && isMatch(s.substring(1, s.length), p.substring(1, p.length))
+  );
 };
+
+console.log(isMatch('aa', 'a'));
+console.log(isMatch('aa', 'a*'));
+console.log(isMatch('aa', 'b*aa'));
+console.log(isMatch('ab', '.*'));
+console.log(isMatch('aaa', 'a*a'));
 
 /**
  * ERROR: still error
