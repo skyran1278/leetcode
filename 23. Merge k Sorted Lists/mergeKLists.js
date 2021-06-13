02bba44d1f5cc14fa4cf2ba95178e9fd293fb537
ran <skyran1278@gmail.com>
2021-06-13 20:09:45 +0800

    優化 mergeKLists

--- 23. Merge k Sorted Lists/mergeKLists.js
+++ 23. Merge k Sorted Lists/mergeKLists.js
@@ -34,6 +34,30 @@ const listNodeToArray = (listNode) => {
 };
 
 /**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const mergeTwoLists = (l1, l2) => {
+  const list = new ListNode();
+  let tail = list;
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      tail.next = new ListNode(l1.val);
+      l1 = l1.next;
+    } else {
+      tail.next = new ListNode(l2.val);
+      l2 = l2.next;
+    }
+    tail = tail.next;
+  }
+
+  tail.next = l1 || l2;
+
+  return list.next;
+};
+
+/**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
@@ -45,6 +69,29 @@ const listNodeToArray = (listNode) => {
  * @return {ListNode}
  */
 const mergeKLists = (lists) => {
+  if (lists.length === 0) {
+    return null;
+  }
+  while (lists.length > 1) {
+    const a = lists.shift();
+    const b = lists.shift();
+    lists.push(mergeTwoLists(a, b));
+  }
+  return lists.shift();
+};
+
+const mergeKListsV2 = (lists) => {
+  const list = new ListNode(); // 0 -> null
+  const tail = list; // 0 -> null
+
+  while (lists.length !== 0) {
+    // null
+    tail.next = mergeTwoLists(tail.next, lists.shift()) || tail.next;
+  }
+  return list.next;
+};
+
+const mergeKListsV1 = (lists) => {
   const list = new ListNode();
   let tail = list;
   lists = lists.filter((list) => list);
