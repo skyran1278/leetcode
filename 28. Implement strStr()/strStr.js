8a8bb2b6a01b7515e6a60db07a85b20e249edad3
ran <skyran1278@gmail.com>
2021-06-25 17:23:21 +0800

    28. Implement strStr()

---
+++ 28. Implement strStr()/strStr.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = (haystack, needle) => {
+  if (needle === '') {
+    return 0;
+  }
+  for (let i = 0; i < haystack.length; i += 1) {
+    if (
+      haystack[i] === needle[0] &&
+      haystack.slice(i, i + needle.length) === needle
+    ) {
+      return i;
+    }
+  }
+  return -1;
+};
