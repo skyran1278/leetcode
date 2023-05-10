8579c864e8eed85d09e555114cd775bb11cd8bf7
ran <skyran1278@gmail.com>
2023-05-11 06:13:07 +0800

    chore: add eslint env setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,10 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
