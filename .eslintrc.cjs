d62261de1861cb0939187b1c423176b8e6b17470
ran <skyran1278@gmail.com>
2023-05-16 06:58:43 +0800

    eslint: update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,18 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
+    es2022: true,
     node: true,
     jest: true,
   },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
 };
