7c729aa564c11c05cb7f7073a7d565842baced42
ran <skyran1278@gmail.com>
2023-05-22 05:21:43 +0800

    feat: stringCompression add another method

--- Cracking the Coding Interview/1 Arrays and Strings/1.6 String Compression/stringCompression.test.ts
+++ Cracking the Coding Interview/1 Arrays and Strings/1.6 String Compression/stringCompression.test.ts
@@ -1,8 +1,27 @@
 // test stringCompression
 //   ✓ should return a2b1c5a3 for aabcccccaaa
-import { stringCompression } from './stringCompression';
+import { stringCompression, stringCompression2 } from './stringCompression';
 
 test('stringCompression', () => {
   expect(stringCompression('aabcccccaaa')).toEqual('a2b1c5a3');
   expect(stringCompression('abca')).toEqual('abca');
+
+  console.time('stringCompression if can be compressed');
+  stringCompression('abcaaaaaaa'.repeat(1000000));
+  console.timeEnd('stringCompression if can be compressed');
+  console.time('stringCompression if cannot be compressed');
+  stringCompression('abca'.repeat(1000000));
+  console.timeEnd('stringCompression if cannot be compressed');
+});
+
+test('stringCompression2', () => {
+  expect(stringCompression2('aabcccccaaa')).toEqual('a2b1c5a3');
+  expect(stringCompression2('abca')).toEqual('abca');
+
+  console.time('stringCompression2 if can be compressed');
+  stringCompression2('abcaaaaaaa'.repeat(1000000));
+  console.timeEnd('stringCompression2 if can be compressed');
+  console.time('stringCompression2 if cannot be compressed');
+  stringCompression2('abca'.repeat(1000000));
+  console.timeEnd('stringCompression2 if cannot be compressed');
 });
--- Cracking the Coding Interview/1 Arrays and Strings/1.6 String Compression/stringCompression.ts
+++ Cracking the Coding Interview/1 Arrays and Strings/1.6 String Compression/stringCompression.ts
@@ -1,8 +1,8 @@
 // String Compression: Implement a method to perform basic string compression using the counts of repeated characters.
 // For example, the string aabcccccaaa would become a2b1c5a3,
 // If the "compressed" string would not become smaller than the original string, your method should return the original string.
-
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
 export const stringCompression = (str: string) => {
   let compressed = '';
   let count = 0;
@@ -14,4 +14,37 @@ export const stringCompression = (str: string) => {
     }
   }
   return compressed.length < str.length ? compressed : str;
+};
+
+// optimized by preallocating the string length
+// 沒有比較好，因為 js 無法預先分配記憶體空間，所以這樣反而會比較慢
+export const stringCompression2 = (str: string) => {
+  const compressedLength = getCompressedLength(str);
+  if (compressedLength >= str.length) return str;
+  const compressed = new Array(compressedLength);
+  let count = 0;
+  for (let i = 0, j = 0; i < str.length; i++, j++) {
+    count++;
+    if (i + 1 >= str.length || str[i] !== str[i + 1]) {
+      compressed[j] = str[i];
+      for (const char of count.toString()) {
+        compressed[++j] = char;
+      }
+      count = 0;
+    }
+  }
+  return compressed.join('');
+};
+
+const getCompressedLength = (str: string) => {
+  let compressedLength = 0;
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    count++;
+    if (i + 1 >= str.length || str[i] !== str[i + 1]) {
+      compressedLength += 1 + count.toString().length;
+      count = 0;
+    }
+  }
+  return compressedLength;
 };
