5e169039d5060b1d418a0c8225bfe54bcf57cd95
skyran1278 <skyran1278@gmail.com>
2023-10-23 21:29:54 +0800

    feat(1.4): 建表處理

--- Cracking the Coding Interview/1 Arrays and Strings/1.4 Palindrome Permutation/palindromePermutation.ts
+++ Cracking the Coding Interview/1 Arrays and Strings/1.4 Palindrome Permutation/palindromePermutation.ts
@@ -1,4 +1,7 @@
-// Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palin- drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+// Palindrome Permutation:
+// Given a string, write a function to check if it is a permutation of a palindrome.
+// A palindrome is a word or phrase that is the same forwards and backwards.
+// A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
 // Input: Tact Coa
 // Output: True (permutations: "taco cat", "atco cta", etc.)
 export const isPalindromePermutation = (str: string): boolean => {
@@ -27,11 +30,7 @@ export const isPalindromePermutation2 = (str: string): boolean => {
   for (const upperChar of str) {
     const char = upperChar.toLowerCase();
     if (char === ' ') continue;
-    if (charMap.has(char)) {
-      charMap.set(char, charMap.get(char)! + 1);
-    } else {
-      charMap.set(char, 1);
-    }
+    charMap.set(char, (charMap.get(char) ?? 0) + 1);
   }
   for (const value of charMap.values()) {
     if (value % 2 !== 0) oddCount++;
