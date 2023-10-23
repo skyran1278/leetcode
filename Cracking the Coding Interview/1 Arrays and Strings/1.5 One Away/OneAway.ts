b087b3859454790555c7b154098906d6137820b1
skyran1278 <skyran1278@gmail.com>
2023-10-23 21:30:27 +0800

    feat(1.5): 可讀性是比較來的

--- Cracking the Coding Interview/1 Arrays and Strings/1.5 One Away/OneAway.ts
+++ Cracking the Coding Interview/1 Arrays and Strings/1.5 One Away/OneAway.ts
@@ -1,9 +1,12 @@
-// One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+// One Away: There are three types of edits that can be performed on strings:
+// insert a character, remove a character, or replace a character.
+// Given two strings, write a function to check if they are one edit (or zero edits) away.
 // EXAMPLE
 // pale, ple -> true
 // pales, pale -> true
 // pale, bale -> true
 // pale, bake -> false
+// 第二種方法的可讀性更好
 
 enum PerformedType {
   Insert,
