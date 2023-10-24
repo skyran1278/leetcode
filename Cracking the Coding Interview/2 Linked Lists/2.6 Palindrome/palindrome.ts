9efe79a22a096d6b9f1c978b6f01d01f4e23d1a9
skyran1278 <skyran1278@gmail.com>
2023-10-24 22:38:24 +0800

    feat(2.6): use two pointer

--- Cracking the Coding Interview/2 Linked Lists/2.6 Palindrome/palindrome.ts
+++ Cracking the Coding Interview/2 Linked Lists/2.6 Palindrome/palindrome.ts
@@ -16,3 +16,27 @@ export const isPalindrome = (head: Node | null): boolean => {
   }
   return true;
 };
+
+export const isPalindrome2 = (head: Node | null): boolean => {
+  const arr = [];
+  let fastRunner = head;
+  let slowRunner = head;
+  while (slowRunner && fastRunner && fastRunner.next) {
+    arr.push(slowRunner.value);
+    slowRunner = slowRunner.next;
+    fastRunner = fastRunner.next.next;
+  }
+
+  if (slowRunner && fastRunner) {
+    slowRunner = slowRunner.next;
+  }
+
+  while (slowRunner) {
+    const top = arr.pop();
+    if (top !== slowRunner.value) {
+      return false;
+    }
+    slowRunner = slowRunner.next;
+  }
+  return true;
+};
