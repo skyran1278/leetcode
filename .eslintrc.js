cf6020935f367037ac2caead3cbd43ee77963f31
skyran1278 <skyran1278@gmail.com>
2024-06-12 04:20:55 +0800

    config(root): update ESLint config to use .eslintrc.js
    - rename .eslintrc.cjs to .eslintrc.js for better compatibility
    - improve comments for environment, parserOptions, and no-shadow rule
    - update .eslintignore to reflect the renamed ESLint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    es2022: true,
-    node: true,
-    jest: true,
+    // https://eslint.org/docs/latest/use/configure/language-options#specifying-environments
+    es2022: true, // adds all ECMAScript 2022 globals and automatically sets the ecmaVersion parser option to 13.
+    node: true, // Node.js global variables and Node.js scoping.
+    jest: true, // Jest global variables.
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
+    // https://typescript-eslint.io/architecture/parser/
+    project: true, // find the tsconfig.json nearest each source file
+    tsconfigRootDir: __dirname, // provide the root directory for relative TSConfig paths specified in the project option
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -27,6 +29,8 @@ module.exports = {
         null: 'ignore',
       },
     ],
+    // https://typescript-eslint.io/rules/no-shadow/
+    // Note: you must disable the base rule as it can report incorrect errors
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
     '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
