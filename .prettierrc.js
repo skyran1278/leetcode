5a9231ea8e304fb55998cae79818a7556ff7fb89
skyran1278 <skyran1278@gmail.com>
2024-06-12 04:21:47 +0800

    chore(config): migrate Prettier configuration to JS file
    - replace .prettierrc with .prettierrc.js for extended configuration options
    - add plugins for embed, SQL, and import sorting
    - enforce single quotes, trailing commas, and specific SQL formatting rules

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// typescript
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: [
+    'prettier-plugin-embed',
+    'prettier-plugin-sql',
+    'prettier-plugin-import-sort', // prettier-plugin-import-sort should be place after prettier-plugin-embed
+  ],
+  language: 'postgresql',
+  keywordCase: 'upper',
+  expressionWidth: 1000,
+};
