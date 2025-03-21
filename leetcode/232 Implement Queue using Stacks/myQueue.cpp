76c9e7c51d1c2366720fc2b9ef9bb56e97b7d377
skyran1278 <skyran1278@gmail.com>
2025-03-21 09:48:16 +0800

    feat(queue): implement queue using two stacks
    adds MyQueue class with push, pop, peek, and empty methods
    utilizes two stacks to manage queue operations
    
    resolves leetcode problem 232

---
+++ leetcode/232 Implement Queue using Stacks/myQueue.cpp
@@ -0,0 +1,46 @@
+#include <stack>
+
+/**
+ * @brief push: O(n), other: O(1)
+ * 14 m 28 s
+ */
+class MyQueue {
+ public:
+  std::stack<int> stack1;
+  std::stack<int> stack2;
+
+  MyQueue() {}
+
+  void push(int x) {
+    while (!stack2.empty()) {
+      stack1.push(stack2.top());
+      stack2.pop();
+    }
+
+    stack2.push(x);
+
+    while (!stack1.empty()) {
+      stack2.push(stack1.top());
+      stack1.pop();
+    }
+  }
+
+  int pop() {
+    int x = stack2.top();
+    stack2.pop();
+    return x;
+  }
+
+  int peek() { return stack2.top(); }
+
+  bool empty() { return stack2.empty(); }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
