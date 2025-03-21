adb795add4cf98946d052022c3cb1ce768fa1df0
skyran1278 <skyran1278@gmail.com>
2025-03-21 09:27:06 +0800

    feat: implement MinStack class for O(1) min retrieval
    - add MinStack class with push, pop, top, and getMin methods
    - ensure constant time complexity for minimum value retrieval

---
+++ leetcode/155. Min Stack/minStack.cpp
@@ -0,0 +1,40 @@
+#include <stack>
+
+/**
+ * @brief O(1)
+ * 18m 57s
+ */
+class MinStack {
+ public:
+  std::stack<int> intStack;
+  std::stack<int> minStack;
+
+  MinStack() { minStack.push(INT_MAX); }
+
+  void push(int val) {
+    if (val <= minStack.top()) {
+      minStack.push(val);
+    }
+    intStack.push(val);
+  }
+
+  void pop() {
+    if (intStack.top() == minStack.top()) {
+      minStack.pop();
+    }
+    intStack.pop();
+  }
+
+  int top() { return intStack.top(); }
+
+  int getMin() { return minStack.top(); }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
