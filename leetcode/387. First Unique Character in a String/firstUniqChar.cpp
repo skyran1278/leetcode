a444665999b6302894438cbc645870c8de125e55
skyran1278 <skyran1278@gmail.com>
2025-03-24 20:03:46 +0800

    feat: add solution for finding first unique character in a string
    - implement algorithm to find the first unique character in a string
    - add main function to test the solution

---
+++ leetcode/387. First Unique Character in a String/firstUniqChar.cpp
@@ -0,0 +1,30 @@
+#include <array>
+#include <climits>
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief  28 m 28 s
+ * O(n)
+ */
+class Solution {
+ public:
+  int firstUniqChar(string s) {
+    int alphabet[26] = {};
+    for (size_t i = 0; i < s.size(); i++) {
+      alphabet[s[i] - 'a']++;
+    }
+    for (size_t i = 0; i < s.size(); i++) {
+      if (alphabet[s[i] - 'a'] == 1) {
+        return i;
+      }
+    }
+    return -1;
+  }
+};
+
+int main() {
+  Solution s;
+  s.firstUniqChar("leetcode");
+}
