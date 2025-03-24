4e9777a55ac50b0f6770070eb32d568aabab6d31
skyran1278 <skyran1278@gmail.com>
2025-03-24 16:52:33 +0800

    feat: add median finder implementation
    - introduce MedianFinder class with addNum and findMedian methods
    - add implementation using min-heap and max-heap for efficient median calculation

---
+++ leetcode/295. Find Medium from Data Stream/medianFinder.cpp
@@ -0,0 +1,67 @@
+#include <queue>
+
+using namespace std;
+
+/**
+ * @brief 23 m 32 s
+ * addNum: O(logn)
+ * findMedian: O(1)
+ */
+class MedianFinder {
+ private:
+  // Initialize a min heap
+  priority_queue<double, vector<double>, greater<double>> minHeap;
+  // Initialize a max heap
+  priority_queue<double, vector<double>, less<double>> maxHeap;
+
+  double medium = LONG_MAX;
+
+ public:
+  MedianFinder() {
+    minHeap.push(LONG_MAX);
+    maxHeap.push(LONG_MIN);
+  }
+
+  void addNum(int num) {
+    if (medium == LONG_MAX) {
+      double large = minHeap.top();
+      double small = maxHeap.top();
+
+      if (num > large) {
+        medium = large;
+        minHeap.pop();
+        minHeap.push(num);
+      } else if (num < small) {
+        medium = small;
+        maxHeap.pop();
+        maxHeap.push(num);
+      } else {
+        medium = num;
+      }
+    } else {
+      if (num > medium) {
+        minHeap.push(num);
+        maxHeap.push(medium);
+      } else {
+        maxHeap.push(num);
+        minHeap.push(medium);
+      }
+      medium = LONG_MAX;
+    }
+  }
+
+  double findMedian() {
+    if (medium != LONG_MAX) {
+      return medium;
+    }
+
+    return (maxHeap.top() + minHeap.top()) / 2;
+  }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
