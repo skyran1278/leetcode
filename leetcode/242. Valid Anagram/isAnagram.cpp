eb1674f2a71b89924e6efa40af0af9bd6f9e938e
skyran1278 <skyran1278@gmail.com>
2025-03-24 18:52:50 +0800

    feat: add isAnagram function for checking anagrams
    - implement isAnagram function to check if two strings are anagrams
    - use an array to count character frequencies
    - return false if character counts mismatch

---
+++ leetcode/242. Valid Anagram/isAnagram.cpp
@@ -0,0 +1,35 @@
+#include <array>
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief 5 m 56 s
+ * O(n)
+ */
+class Solution {
+ public:
+  bool isAnagram(string s, string t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
+    int alphabet[26] = {0};
+
+    for (size_t i = 0; i < s.size(); i++) {
+      char c = s[i];
+      alphabet[c - 'a'] += 1;
+    }
+
+    for (size_t i = 0; i < t.size(); i++) {
+      char c = t[i];
+      alphabet[c - 'a'] -= 1;
+
+      if (alphabet[c - 'a'] < 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
