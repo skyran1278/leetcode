cb7b7bd1f9a50b0044f735b3549625027ddfcad8
skyran1278 <skyran1278@gmail.com>
2025-04-01 07:04:34 +0800

    feat(find-duplicate): add solution for finding duplicate in array
    - implement Floyd's Tortoise and Hare algorithm to detect duplicates
    - include a main function with test case for demonstration

---
+++ leetcode/287. Find the Duplicate Number/findDuplicate.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief hard, I read answer to solve this problem
+ * O(n)
+ */
+class Solution {
+ public:
+  int findDuplicate(vector<int>& nums) {
+    int slowRunner = 0;
+    int fastRunner = 0;
+
+    do {
+      slowRunner = nums[slowRunner];
+      fastRunner = nums[nums[fastRunner]];
+    } while (slowRunner != fastRunner);
+
+    slowRunner = 0;
+
+    while (slowRunner != fastRunner) {
+      slowRunner = nums[slowRunner];
+      fastRunner = nums[fastRunner];
+    }
+
+    return slowRunner;
+  }
+};
+
+int main() {
+  Solution s;
+
+  vector<int> s1 = {1, 3, 4, 2, 2};
+  s.findDuplicate(s1);
+}
