0683dd870255a8c0d204d167256277a5e9ffbedc
skyran1278 <skyran1278@gmail.com>
2025-04-12 08:23:11 +0800

    feat(tree): add balanced binary tree check implementations
    - implement O(n) and O(n^2) algorithms to check if a binary tree is balanced
    - include main function with test cases for validation
    - update VS Code settings with new C++ header mapping for 'climits'

---
+++ leetcode/110. Balanced Binary Tree/isBalanced.cpp
@@ -0,0 +1,89 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+/**
+ * @brief 54 m 35 s
+ * O(n)
+ */
+class Solution {
+ public:
+  bool isBalanced(TreeNode *root) { return depth(root) >= 0; }
+
+  int depth(TreeNode *root) {
+    if (root == nullptr) return 0;
+
+    int leftDepth = depth(root->left);
+    int rightDepth = depth(root->right);
+
+    int d = abs(leftDepth - rightDepth);
+
+    if (leftDepth == -1 || rightDepth == -1 || d > 1) {
+      return -1;
+    }
+
+    return max(leftDepth, rightDepth) + 1;
+  }
+};
+
+/**
+ * @brief 54 m 35 s
+ * O(n^2)
+ */
+class Solution2 {
+ public:
+  bool isBalanced(TreeNode *root) {
+    if (root == nullptr) return true;
+
+    int d = depth(root->left) - depth(root->right);
+
+    if (d > 1 || d < -1) {
+      return false;
+    }
+
+    return isBalanced(root->left) && isBalanced(root->right);
+  }
+
+ private:
+  int depth(TreeNode *root) {
+    if (root == nullptr) return 0;
+
+    return max(depth(root->left), depth(root->right)) + 1;
+  }
+};
+
+int main() {
+  Solution s;
+
+  TreeNode *node1 = new TreeNode(4);
+  TreeNode *node2 = new TreeNode(4);
+  TreeNode *node3 = new TreeNode(3, node1, node2);
+  TreeNode *node4 = new TreeNode(3);
+  TreeNode *node5 = new TreeNode(2, node3, node4);
+  TreeNode *node6 = new TreeNode(2);
+  TreeNode *node7 = new TreeNode(1, node5, node6);
+  s.isBalanced(node7);
+}
