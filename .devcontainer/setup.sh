351d09d6bede7c92783681595aa202ec53d4fd2c
skyran1278 <skyran1278@gmail.com>
2025-04-16 02:33:37 +0000

    feat(devcontainer): add Miniconda installation to Dockerfile and update setup script
    - download and install Miniconda based on architecture
    - clean up installer after installation
    - initialize conda in all shells and disable auto-activation of base environment
    - remove conda initialization from setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,9 +2,7 @@
 gcc -v
 g++ -v
 
-# Install conda environment.
-conda init zsh
-conda config --set auto_activate_base false
+# Install python packages.
 conda env create -f environment.yml
 
 # Install node packages.
