171d4724ea802f7a51286cb93240fb4209c66c41
skyran1278 <skyran1278@gmail.com>
2025-04-22 08:51:27 +0000

    feat(regex-matching): implement dynamic programming solution
    - add dynamic programming-based approach for regular expression matching
    - handle wildcard `.` and quantifier `*` with zero or more matches
    - include example test cases in the main function for validation

--- leetcode/10. Regular Expression Matching/isMatch.cpp
+++ leetcode/10. Regular Expression Matching/isMatch.cpp
@@ -1,8 +1,48 @@
 #include <string>
+#include <vector>
 
 using namespace std;
 
+/**
+ * 看解答，沒做出來
+ */
 class Solution {
  public:
-  bool isMatch(string s, string p) {}
+  bool isMatch(string s, string p) {
+    int sLen = s.length();
+    int pLen = p.length();
+
+    vector<vector<bool>> dp(sLen + 1, vector<bool>(pLen + 1));
+
+    // Empty string matches empty pattern
+    dp[0][0] = true;
+
+    for (size_t j = 2; j <= pLen; j++) {
+      if (p[j - 1] == '*') {
+        dp[0][j] = dp[0][j - 2];  // zero
+      }
+    }
+
+    for (size_t i = 1; i <= sLen; i++) {
+      for (size_t j = 1; j <= pLen; j++) {
+        if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else if (p[j - 1] == '*') {
+          dp[i][j] = dp[i][j - 2];  // zero
+          if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+            dp[i][j] = dp[i - 1][j] || dp[i][j];  // one or more
+          }
+        }
+      }
+    }
+
+    return dp[sLen][pLen];
+  }
 };
+
+int main() {
+  Solution s;
+
+  s.isMatch("aa", "a*");
+  s.isMatch("aa", "a");
+}
