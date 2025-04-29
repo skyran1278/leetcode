bf0122a7e037187cd028351bf541bfbb01d037f1
skyran1278 <skyran1278@gmail.com>
2025-04-29 05:18:19 +0000

    refactor(product-of-array-except-self): simplify product calculation logic
    - remove unnecessary total variable and division operation
    - streamline the algorithm to use prefix and suffix products

--- leetcode/238. Product of Array Except Self/Solution.cpp
+++ leetcode/238. Product of Array Except Self/Solution.cpp
@@ -3,17 +3,27 @@
 
 using namespace std;
 
+/**
+ * @brief 沒寫出來
+ * O(n)
+ */
 class Solution {
  public:
   vector<int> productExceptSelf(vector<int>& nums) {
-    int total = 1;
-    for (auto&& num : nums) {
-      num *= num;
+    int n = nums.size();
+    vector<int> result(n);
+
+    result[0] = 1;
+    for (size_t i = 1; i < n; i++) {
+      result[i] = result[i - 1] * nums[i - 1];
     }
 
-    vector<int> result(nums.size());
-    for (size_t i = 0; i < nums.size(); i++) {
-      result[i] = total / nums[i];
+    int rightProduct = 1;
+    for (int i = n - 1; i >= 0; i--) {
+      result[i] = result[i] * rightProduct;
+      rightProduct *= nums[i];
     }
+
+    return result;
   }
 };
