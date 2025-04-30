2e9e5d3d48292f554108f14d65fd0aa61d8f0c57
skyran1278 <skyran1278@gmail.com>
2025-04-30 09:49:23 +0000

    feat(integer-break): implement initial structure for integer break solution
    - define integerBreak function
    - initialize maxN vector for dynamic programming

---
+++ leetcode/343. Integer Break/Solution.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int integerBreak(int n) {
+    vector<int> maxN(n + 1);
+    maxN[2] = 1;
+    for (size_t i = 3; i < n; i++) {
+      /* code */
+    }
+  }
+};
