4f2a0844b0202f53b9f7bf01fd859256da724127
skyran1278 <skyran1278@gmail.com>
2025-05-01 09:58:33 +0000

    feat(integer-break): implement optimized integer break solution
    - refactor logic to use dynamic programming for maximum product calculation
    - improve variable naming for clarity

--- leetcode/343. Integer Break/Solution.cpp
+++ leetcode/343. Integer Break/Solution.cpp
@@ -3,13 +3,39 @@
 
 using namespace std;
 
+/**
+ * @brief 12 m 27 s
+ * O(n^2)
+ * DP
+ */
 class Solution {
  public:
   int integerBreak(int n) {
-    vector<int> maxN(n + 1);
-    maxN[2] = 1;
-    for (size_t i = 3; i < n; i++) {
-      /* code */
+    vector<int> maxProducts(n + 1);
+    maxProducts[1] = 0;
+    maxProducts[2] = 1;
+    for (size_t i = 3; i < n + 1; i++) {
+      int maxProduct = 0;
+      int left = 1;
+      int right = i - 1;
+      while (left <= right) {
+        int maxLeft = max(maxProducts[left], left);
+        int maxRight = max(maxProducts[right], right);
+        if (maxLeft * maxRight > maxProduct) {
+          maxProduct = maxLeft * maxRight;
+        }
+        left++;
+        right--;
+      }
+
+      maxProducts[i] = maxProduct;
     }
+
+    return maxProducts[n];
   }
 };
+
+int main() {
+  Solution s;
+  s.integerBreak(4);
+}
