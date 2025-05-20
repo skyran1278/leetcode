0a935f445bd6dce262cd6500d87620e7407de11d
skyran1278 <skyran1278@gmail.com>
2025-05-20 04:22:51 +0000

    feat(stack): implement triple stack data structure
    - provide push, pop, peek, and isEmpty methods
    - support three stacks within a single array

---
+++ Cracking the Coding Interview/3 Stacks and Queues/3.1 Three in One/Three in One LCCI.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 20 m 35 s
+ *
+ * Simulate
+ */
+class TripleInOne {
+ public:
+  TripleInOne(int stackSize) : _data(stackSize * 3), _stackSize(stackSize) {}
+
+  void push(int stackNum, int value) {
+    if (_tops[stackNum] + 1 < _stackSize) {
+      _data[index(stackNum, ++_tops[stackNum])] = value;
+    }
+  }
+
+  int pop(int stackNum) {
+    if (isEmpty(stackNum)) {
+      return -1;
+    }
+
+    return _data[index(stackNum, _tops[stackNum]--)];
+  }
+
+  int peek(int stackNum) {
+    if (isEmpty(stackNum)) {
+      return -1;
+    }
+
+    return _data[index(stackNum, _tops[stackNum])];
+  }
+
+  bool isEmpty(int stackNum) { return _tops[stackNum] == -1; }
+
+ private:
+  int _tops[3] = {-1, -1, -1};
+  vector<int> _data;
+  int _stackSize;
+
+  int index(int stackNum, int offset) const {
+    return stackNum * _stackSize + offset;
+  }
+};
+
+/**
+ * Your TripleInOne object will be instantiated and called as such:
+ * TripleInOne* obj = new TripleInOne(stackSize);
+ * obj->push(stackNum,value);
+ * int param_2 = obj->pop(stackNum);
+ * int param_3 = obj->peek(stackNum);
+ * bool param_4 = obj->isEmpty(stackNum);
+ */
