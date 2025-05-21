c7d7dbdc0ce802df459bea7ff31bfe5710f26a6a
skyran1278 <skyran1278@gmail.com>
2025-05-21 03:08:37 +0000

    feat(stack): implement stack of plates data structure
    - allow pushing and popping of values across multiple stacks
    - support dynamic stack creation based on capacity

---
+++ Cracking the Coding Interview/3 Stacks and Queues/3.3 Stack of Plates/Solution.cpp
@@ -0,0 +1,74 @@
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 20 m 24 s
+ *
+ */
+class StackOfPlates {
+ public:
+  StackOfPlates(int cap) : cap_(cap) {}
+
+  void push(int val) {
+    if (cap_ == 0) {
+      return;
+    }
+
+    if (stacks_.empty() || stacks_.back().size() == cap_) {
+      stacks_.push_back({});
+    }
+
+    stacks_.back().push(val);
+  }
+
+  int pop() {
+    if (stacks_.empty()) {
+      return -1;
+    }
+
+    stack<int> &lastStack = stacks_.back();
+
+    int value = lastStack.top();
+    lastStack.pop();
+
+    if (lastStack.empty()) {
+      stacks_.pop_back();
+    }
+
+    return value;
+  }
+
+  int popAt(int index) {
+    if (index >= stacks_.size()) {
+      return -1;
+    }
+
+    stack<int> &specificStack = stacks_[index];
+
+    int value = specificStack.top();
+    specificStack.pop();
+
+    if (specificStack.empty()) {
+      stacks_.erase(stacks_.begin() + index);
+    }
+
+    return value;
+  }
+
+ private:
+  vector<stack<int>> stacks_;
+  int cap_;
+};
+
+/**
+ * Your StackOfPlates object will be instantiated and called as such:
+ * StackOfPlates* obj = new StackOfPlates(cap);
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->popAt(index);
+ */
