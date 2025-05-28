c4ded21d619b0305f54d45ac91c76d7b66d25309
skyran1278 <skyran1278@gmail.com>
2025-05-28 09:34:31 +0000

    feat(readme): add README for Minimal Tree problem
    - add initial README outlining the problem statement and hints
    - provide structure for the solution section

---
+++ Cracking the Coding Interview/4 Trees and Graphs/4.2 Minimal Tree/README.md
@@ -0,0 +1,9 @@
+# Issue
+
+## Question
+
+Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algo­rithm to create a binary search tree with minimal height.
+
+Hints: #79, #73, #7 76
+
+## Solution
---
+++ Cracking the Coding Interview/4 Trees and Graphs/4.2 Minimal Tree/Solution.cpp
@@ -0,0 +1,43 @@
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+/**
+ * @brief 15 m 4 s
+ *
+ */
+class Solution {
+ public:
+  TreeNode *sortedArrayToBST(vector<int> &nums) {
+    return dfs(nums, 0, nums.size());
+  }
+
+  TreeNode *dfs(vector<int> &nums, int left, int right) {
+    if (left >= right) {
+      return nullptr;
+    }
+
+    int index = left + (right - left) / 2;
+
+    TreeNode *node = new TreeNode(nums[index]);
+    node->left = dfs(nums, left, index);
+    node->right = dfs(nums, index + 1, right);
+    return node;
+  }
+};
