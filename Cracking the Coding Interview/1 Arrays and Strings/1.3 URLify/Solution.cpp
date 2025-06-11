7b4eb4d6a9bf835392a1692426c5d589db1d7dc8
skyran1278 <skyran1278@gmail.com>
2025-06-11 02:11:30 +0000

    feat(URLify): implement space replacement in string with %20

---
+++ Cracking the Coding Interview/1 Arrays and Strings/1.3 URLify/Solution.cpp
@@ -0,0 +1,30 @@
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 4 m 25 s
+ * O(n)
+ * O(n)
+ */
+class Solution {
+ public:
+  string replaceSpaces(string S, int length) {
+    string result;
+    for (size_t i = 0; i < length; i++) {
+      if (S[i] == ' ') {
+        result += "%20";
+      } else {
+        result += S[i];
+      }
+    }
+
+    return result;
+  }
+};
