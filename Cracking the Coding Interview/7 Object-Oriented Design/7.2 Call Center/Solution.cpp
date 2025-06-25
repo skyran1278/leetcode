1ba1bb724fb7a708d6035f3df1beff6cff9e3d90
skyran1278 <skyran1278@gmail.com>
2025-06-25 09:55:38 +0000

    feat(Call Center): add initial implementation of call center classes
    - introduce classes for handling call center operations
    - set up basic structure for future enhancements

---
+++ Cracking the Coding Interview/7 Object-Oriented Design/7.2 Call Center/Solution.cpp
@@ -0,0 +1,10 @@
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
