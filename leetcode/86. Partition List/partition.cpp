860e6438b592aa5667269ec7cc1cc3c5f1d43697
skyran1278 <skyran1278@gmail.com>
2025-08-29 09:54:18 +0000

    1

--- leetcode/86. Partition List/partition.cpp
+++ leetcode/86. Partition List/partition.cpp
@@ -19,26 +19,34 @@ struct ListNode {
 
 class Solution {
  public:
-  ListNode* partition(ListNode* head, int x) {
-    ListNode* l1 = new ListNode();
-    ListNode* l1Runner = l1;
-    ListNode* l2 = new ListNode();
-    ListNode* l2Runner = l2;
+  ListNode* partition(ListNode* head,
+                      int x) {  // head = [1, 4, 3, 2, 5, 2], x = 3
+    ListNode* current = head;   // node = [1, 4, 3, 2, 5, 2]
+    ListNode* tail = head;      // tail = [1, 4, 3, 2, 5, 2]
 
-    while (head != nullptr) {
-      if (head->val < x) {
-        l1Runner->next = head;
-        l1Runner = l1Runner->next;
-      } else {
-        l2Runner->next = head;
-        l2Runner = l2Runner->next;
+    // find the x
+    while (current != nullptr) {
+      if (current->val == x) {
+        break;
       }
-      head = head->next;
+      current = current->next;
     }
 
-    l1Runner->next = l2->next;
-    l2Runner->next = nullptr;
+    // current = [3, 2, 5, 2]
+    while (current != nullptr && current->next != nullptr) {
+      // 1. next = [2, 5, 2]
+      ListNode* next = current->next;
+      if (next->val < x) {
+        // 1. current = [3, 5, 2]
+        current->next = next->next;
 
-    return l1->next;
+        //
+        next->next = tail->next;
+        tail->next = next;
+      }
+      current = current->next;
+    }
+
+    return head;
   }
 };
