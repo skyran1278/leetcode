96d545ecf3c6641b3ee943ccc899accf15827349
skyran1278 <skyran1278@gmail.com>
2025-09-01 07:53:51 +0000

    fix(partition): terminate greater list before merge
    - add step-by-step comments and examples to clarify pointer movement

--- leetcode/86. Partition List/partition.cpp
+++ leetcode/86. Partition List/partition.cpp
@@ -19,6 +19,8 @@ struct ListNode {
 
 class Solution {
  public:
+  // head = [1, 4, 3, 2, 5, 2], x = 3
+  // head = [2, 1], x = 2
   ListNode* partition(ListNode* head, int x) {
     ListNode* l1 = new ListNode();
     ListNode* l1Runner = l1;
@@ -27,17 +29,38 @@ class Solution {
 
     while (head != nullptr) {
       if (head->val < x) {
+        // 1. l1Runner = [0, 1, 4, 3, 2, 5, 2]
+        // 4. l1Runner = [1, 2, 5, 2]
+        // 6. l1Runner = [2, 2]
         l1Runner->next = head;
+        // 1. l1Runner = [1, 4, 3, 2, 5, 2]
+        // 4. l1Runner = [2, 5, 2]
+        // 6. l1Runner = [2]
         l1Runner = l1Runner->next;
       } else {
+        // 2. l2Runner = [0, 4, 3, 2, 5, 2]
+        // 3. l2Runner = [4, 3, 2, 5, 2]
+        // 5. l2Runner = [3, 5, 2]
         l2Runner->next = head;
+        // 2. l2Runner = [4, 3, 2, 5, 2]
+        // 3. l2Runner = [3, 2, 5, 2]
+        // 5. l2Runner = [5, 2]
         l2Runner = l2Runner->next;
       }
+
+      // 1. head = [4, 3, 2, 5, 2]
+      // 2. head = [3, 2, 5, 2]
+      // 3. head = [2, 5, 2]
+      // 4. head = [5, 2]
+      // 5. head = [2]
+      // 6. head = []
       head = head->next;
     }
 
-    l1Runner->next = l2->next;
+    // l2Runner = [5]
     l2Runner->next = nullptr;
+    // l1Runner = [2, 4, 3, 5]
+    l1Runner->next = l2->next;
 
     return l1->next;
   }
