9cc24da146b260dc0a63dca82956c0453faa1f30
skyran1278 <skyran1278@gmail.com>
2025-09-03 10:43:54 +0000

    refactor(min-stack): improve encapsulation and init
    - make internal stacks private and use clearer names
    - simplify namespace qualification to reduce verbosity
    - initialize sentinel via builtin limit to avoid extra header
    - add inline examples to document expected behavior

--- leetcode/155. Min Stack/minStack.cpp
+++ leetcode/155. Min Stack/minStack.cpp
@@ -1,33 +1,37 @@
 #include <stack>
 
+using namespace std;
+
 /**
  * @brief 18 m 57 s
+ * @brief 8 m 54 s
  * O(1)
  */
 class MinStack {
  public:
-  std::stack<int> intStack;
-  std::stack<int> minStack;
-
-  MinStack() { minStack.push(INT_MAX); }
+  MinStack() { minStack_.push(__INT_MAX__); }
 
   void push(int val) {
-    if (val <= minStack.top()) {
-      minStack.push(val);
+    if (val <= minStack_.top()) {
+      minStack_.push(val);
     }
-    intStack.push(val);
+    normalStack_.push(val);
   }
 
   void pop() {
-    if (intStack.top() == minStack.top()) {
-      minStack.pop();
+    if (normalStack_.top() == minStack_.top()) {
+      minStack_.pop();
     }
-    intStack.pop();
+    normalStack_.pop();
   }
 
-  int top() { return intStack.top(); }
+  int top() { return normalStack_.top(); }
 
-  int getMin() { return minStack.top(); }
+  int getMin() { return minStack_.top(); }
+
+ private:
+  stack<int> normalStack_;
+  stack<int> minStack_;
 };
 
 /**
@@ -37,4 +41,22 @@ class MinStack {
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
+ *
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ *
+ * normalStack_ = []
+ * minStack_ = [__INT_MAX__]
+ *
+ * normalStack_ = [-2]
+ * minStack_ = [-2, __INT_MAX__]
+ *
+ * normalStack_ = [0, -2]
+ * minStack_ = [-2, __INT_MAX__]
+ *
+ * normalStack_ = [-3, 0, -2]
+ * minStack_ = [-3, -2, __INT_MAX__]
+ *
+ * normalStack_ = [0, -2]
+ * minStack_ = [-2, __INT_MAX__]
  */
