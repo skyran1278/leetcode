dae386a473c61193da9df1187aa68d4375a2bd38
skyran1278 <skyran1278@gmail.com>
2025-09-08 02:21:42 +0000

    refactor(isomorphic): rename maps and add comments
    - improve readability with shorter symmetric names
    - add inline examples illustrating bidirectional mapping
    - ensure no functional change

--- leetcode/205. Isomorphic Strings/isIsomorphic.cpp
+++ leetcode/205. Isomorphic Strings/isIsomorphic.cpp
@@ -5,28 +5,35 @@ using namespace std;
 
 /**
  * @brief 28 m 20 s
+ * @brief 21 m 53 s
  * O(n)
  */
 class Solution {
  public:
+  // s = "foo", t = "bar"
+  // s = "bar", t = "foo"
   bool isIsomorphic(string s, string t) {
-    unordered_map<char, char> stMapping;
-    unordered_map<char, char> tsMapping;
+    unordered_map<char, char> stMap;
+    unordered_map<char, char> tsMap;
 
     for (size_t i = 0; i < s.size(); i++) {
       char sChar = s[i];
       char tChar = t[i];
 
-      if (stMapping.count(sChar) > 0 && stMapping[sChar] != tChar) {
+      if (stMap.count(sChar) > 0 && stMap[sChar] != tChar) {
         return false;
       }
 
-      if (tsMapping.count(tChar) > 0 && tsMapping[tChar] != sChar) {
+      if (tsMap.count(tChar) > 0 && tsMap[tChar] != sChar) {
         return false;
       }
 
-      stMapping[sChar] = tChar;
-      tsMapping[tChar] = sChar;
+      // stMap = b: f
+      // stMap = b: f, a: o
+      stMap[sChar] = tChar;
+      // tsMap = f: b
+      // tsMap = f: b, o: a
+      tsMap[tChar] = sChar;
     }
 
     return true;
