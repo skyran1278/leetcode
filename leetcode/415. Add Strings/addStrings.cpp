6ee587c9d806908427a190fcaff975e6cb47f5f4
skyran1278 <skyran1278@gmail.com>
2025-09-13 04:25:46 +0000

    refactor: improve string addition algorithm implementation
    - optimize variable naming and type safety with explicit casting
    - simplify modulo operation for digit extraction
    - enhance code readability with better variable names
    - add example comment for algorithm understanding
    - update timing documentation for performance tracking

--- leetcode/415. Add Strings/addStrings.cpp
+++ leetcode/415. Add Strings/addStrings.cpp
@@ -5,25 +5,27 @@ using namespace std;
 
 /**
  * @brief 21 m 49 s
+ * @brief 14 m 0 s
  * O(n)
+ * O(1)
  */
 class Solution {
  public:
+  // num1 = "11", num2 = "123"
   string addStrings(string num1, string num2) {
-    string result = "";
+    string result;
 
-    int i = num1.size() - 1;
-    int j = num2.size() - 1;
+    int length1 = (int)num1.size() - 1;
+    int length2 = (int)num2.size() - 1;
 
     int carry = 0;
-    while (i >= 0 || j >= 0 || carry == 1) {
-      int n1 = i >= 0 ? num1[i--] - '0' : 0;
-      int n2 = j >= 0 ? num2[j--] - '0' : 0;
+    while (length1 >= 0 || length2 >= 0 || carry > 0) {
+      int i1 = length1 >= 0 ? num1[(size_t)length1--] - '0' : 0;
+      int i2 = length2 >= 0 ? num2[(size_t)length2--] - '0' : 0;
 
-      int sum = carry + n1 + n2;
-
+      int sum = carry + i1 + i2;
       carry = sum >= 10 ? 1 : 0;
-      result.push_back((sum >= 10 ? sum - 10 : sum) + '0');
+      result.push_back('0' + (char)sum % 10);
     }
 
     reverse(result.begin(), result.end());
