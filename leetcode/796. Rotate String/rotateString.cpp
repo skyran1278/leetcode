061f85095b0bfe9c9410598e4dea03444aa2dc64
skyran1278 <skyran1278@gmail.com>
2025-09-13 11:30:28 +0000

    docs: update timing and add test cases for rotate string
    - improves documentation with updated timing benchmark
    - adds example test cases to clarify expected input/output behavior

--- leetcode/796. Rotate String/rotateString.cpp
+++ leetcode/796. Rotate String/rotateString.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 /**
  * @brief 11 m 5 s
+ * @brief 8 m 0 s
+ * O(n)
  * O(n)
  */
 class Solution {
  public:
+  // s = "abcde", goal = "cdeab"
+  // s = "aa", goal = "aaa"
   bool rotateString(string s, string goal) {
     if (s.length() != goal.length()) {
       return false;
