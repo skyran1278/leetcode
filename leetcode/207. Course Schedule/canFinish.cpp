8e1ac89520b3774872bbcf528b6315de95a2a4bf
skyran1278 <skyran1278@gmail.com>
2025-09-27 07:31:59 +0000

    refactor: improve code clarity and add inline comments
    - updates time complexity documentation to be more precise
    - adds inline comments explaining algorithm steps with examples
    - changes variable types from int to size_t for consistency
    - reorders variable declarations for better logical flow

--- leetcode/207. Course Schedule/canFinish.cpp
+++ leetcode/207. Course Schedule/canFinish.cpp
@@ -5,29 +5,32 @@ using namespace std;
 
 /**
  * @brief 1 hrs 53 m 18 s
- * O(m + n)
+ * @brief 15 m 57 s
+ * O(V + E) (courses + prerequisites)
+ * O(V + E) (adjacency list + recursion/queue)
  */
 class Solution {
  public:
+  // numCourses = 2, prerequisites = [[1,0]]
   bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    vector<vector<int>> graph(numCourses);  // adjacency list
     vector<int> inDegree(numCourses, 0);
+    vector<vector<int>> graph(numCourses);  // adjacency list
 
     for (auto&& pre : prerequisites) {
-      graph[pre[1]].push_back(pre[0]);
-      inDegree[pre[0]]++;
+      graph[pre[1]].push_back(pre[0]);  // graph = 0: [1]
+      inDegree[pre[0]]++;               // inDegree = [0, 1]
     }
 
-    queue<int> q;
-    for (size_t i = 0; i < inDegree.size(); i++) {
+    queue<size_t> q;
+    for (size_t i = 0; i < numCourses; i++) {
       if (inDegree[i] == 0) {
-        q.push(i);
+        q.push(i);  // q = 0
       }
     }
 
     int completedCourse = 0;
     while (!q.empty()) {
-      int course = q.front();
+      size_t course = q.front();
       q.pop();
       completedCourse++;
 
