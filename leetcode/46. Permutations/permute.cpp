3c8b93be829eb8ba683532c37e6a765dc21219bc
skyran1278 <skyran1278@gmail.com>
2025-09-30 06:25:23 +0000

    docs: improve time complexity comment clarity
    - clarifies recursion depth as O(n) separate from output size
    - consolidates explanation into single concise line

--- leetcode/46. Permutations/permute.cpp
+++ leetcode/46. Permutations/permute.cpp
@@ -6,8 +6,7 @@ using namespace std;
  * @brief 16 m 37 s
  * @brief 23 m 30 s
  * O(n * n!)
- * n! for the total number of permutations.
- * n for copying each permutation into the result list.
+ * O(n) for recursion depth, plus O(n * n!) output.
  */
 class Solution {
  public:
