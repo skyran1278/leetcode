c62699962114065d63a6c15e5f29cabe8a380e4a
skyran1278 <skyran1278@gmail.com>
2025-10-14 01:52:48 +0000

    refactor: replace member variable with local parameter
    - removes class member variable inorderMap_ in favor of local scope
    - passes inorderMap as parameter to helper function instead of accessing member
    - improves encapsulation by eliminating shared state between method calls

--- leetcode/105. Construct Binary Tree from Preorder and Inorder Traversal/buildTree.cpp
+++ leetcode/105. Construct Binary Tree from Preorder and Inorder Traversal/buildTree.cpp
@@ -23,34 +23,34 @@ class Solution {
  public:
   // preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
   TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    unordered_map<int, int> inorderMap;
     for (size_t i = 0; i < inorder.size(); i++) {
-      inorderMap_[inorder[i]] = i;
+      inorderMap[inorder[i]] = i;
     }
 
-    return _buildTree(preorder, inorder, 0, 0, preorder.size());
+    return _buildTree(preorder, inorderMap, 0, 0, preorder.size());
   }
 
  private:
-  unordered_map<int, int> inorderMap_;
-
   // preIdx = 0, inIdx = 0, count = 5
   // preIdx = 1, inIdx = 0, count = 1
   // preIdx = 2, inIdx = 2, count = 3
-  TreeNode *_buildTree(vector<int> &preorder, vector<int> &inorder,
-                       size_t preIdx, size_t inIdx, int count) {
+  TreeNode *_buildTree(vector<int> &preorder,
+                       unordered_map<int, int> &inorderMap, size_t preIdx,
+                       size_t inIdx, int count) {
     if (count <= 0) {
       return nullptr;
     }
 
     int rootValue = preorder[preIdx];
-    int rootIndex = inorderMap_[rootValue];
+    int rootIndex = inorderMap[rootValue];
 
     int leftCount = rootIndex - inIdx;
     int rightCount = count - leftCount - 1;
 
     TreeNode *root = new TreeNode(rootValue);
-    root->left = _buildTree(preorder, inorder, preIdx + 1, inIdx, leftCount);
-    root->right = _buildTree(preorder, inorder, preIdx + 1 + leftCount,
+    root->left = _buildTree(preorder, inorderMap, preIdx + 1, inIdx, leftCount);
+    root->right = _buildTree(preorder, inorderMap, preIdx + 1 + leftCount,
                              rootIndex + 1, rightCount);
 
     return root;
