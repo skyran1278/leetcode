886c9d84bf136fe6ce4e677db58566241155c381
skyran1278 <skyran1278@gmail.com>
2025-10-21 00:52:55 +0000

    refactor: simplify subsequence check with standard DP approach
    - replaces complex character indexing algorithm with straightforward 2D DP table
    - improves code readability and maintainability by using conventional longest common subsequence pattern
    - reduces algorithmic complexity from preprocessing-heavy approach to direct comparison method

--- leetcode/392. Is Subsequence/isSubsequence2.cpp
+++ leetcode/392. Is Subsequence/isSubsequence2.cpp
@@ -7,37 +7,35 @@ using namespace std;
 /**
  * @brief 我目前仍寫不出來動態規劃的程式，這是看官方解答的
  */
-class Solution2 {
+class Solution {
  public:
+  // s = "abc", t = "ahbgdc"
   bool isSubsequence(string s, string t) {
-    int n = s.size(), m = t.size();
+    int m = s.size(), n = t.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-    vector<vector<int> > f(m + 1, vector<int>(26, 0));
-    for (int i = 0; i < 26; i++) {
-      f[m][i] = m;
-    }
-
-    for (int i = m - 1; i >= 0; i--) {
-      for (int j = 0; j < 26; j++) {
-        if (t[i] == j + 'a')
-          f[i][j] = i;
+    /**
+     *     a h b g d c
+     *   0 0 0 0 0 0 0
+     * a 0 1 1 1 1 1 1
+     * b 0 0 0 2 2 2 2
+     * c 0 0 0 0 0 0 3
+     */
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        if (s[i - 1] == t[j - 1])
+          dp[i][j] = dp[i - 1][j - 1] + 1;
         else
-          f[i][j] = f[i + 1][j];
+          dp[i][j] = dp[i][j - 1];
       }
     }
-    int add = 0;
-    for (int i = 0; i < n; i++) {
-      if (f[add][s[i] - 'a'] == m) {
-        return false;
-      }
-      add = f[add][s[i] - 'a'] + 1;
-    }
-    return true;
+
+    return dp[m][n] == m;
   }
 };
 
 int main() {
-  Solution2 s;
+  Solution s;
   bool s1 = s.isSubsequence("abc", "ahbgdc");
   bool s2 = s.isSubsequence("axc", "ahbgdc");
 
