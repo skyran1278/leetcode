368c7ba1ed092da1818397b4633ee58df71d7b77
skyran1278 <skyran1278@gmail.com>
2025-11-05 03:52:40 +0000

    feat(palindrome): implement solution for palindrome permutation check
    - adds a method to determine if a string can be rearranged to form a palindrome
    - utilizes a hash map to count character occurrences
    - allows for efficient O(n) time complexity

---
+++ Cracking the Coding Interview/1 Arrays and Strings/1.4 Palindrome Permutation/Solution.cpp
@@ -0,0 +1,32 @@
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 6 m 19 s
+ * O(n)
+ * O(Σ)
+ */
+class Solution {
+ public:
+  bool canPermutePalindrome(string s) {
+    unordered_map<char, int> map;
+    for (auto&& c : s) {
+      map[c]++;
+    }
+
+    int oddCount = 0;
+    for (auto&& [c, count] : map) {
+      if (count % 2 == 1) oddCount++;
+    }
+
+    return oddCount <= 1;
+  }
+};
