0874a17c5ad3ccb6b8d2b45c6e66773b2e37e798
skyran1278 <skyran1278@gmail.com>
2025-11-07 10:11:47 +0000

    feat(matrix): implement zero matrix transformation
    - adds functionality to set entire rows and columns to zero
      when a zero is encountered in a matrix
    - optimizes for $O(mn)$ time and $O(m+n)$ space complexity
    - improves support for coding interview practice problems

---
+++ Cracking the Coding Interview/1 Arrays and Strings/1.8 Zero Matrix/Solution.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 11 m 13 s
+ * O(m*n)
+ * O(m+n)
+ */
+class Solution {
+ public:
+  // [
+  //   [0,1,2,0],
+  //   [3,4,5,2],
+  //   [1,3,1,5]
+  // ]
+  void setZeroes(vector<vector<int>>& matrix) {
+    vector<bool> rows(matrix.size());     // [0, 1, 1]
+    vector<bool> cols(matrix[0].size());  // [0, 1, 1, 0]
+    for (size_t row = 0; row < matrix.size(); row++) {
+      for (size_t col = 0; col < matrix[row].size(); col++) {
+        if (matrix[row][col] == 0) {
+          rows[row] = true;
+          cols[col] = true;
+        }
+      }
+    }
+
+    // [
+    //   [0,0,0,0],
+    //   [0,4,5,0],
+    //   [0,3,1,0]
+    // ]
+    for (size_t row = 0; row < matrix.size(); row++) {
+      for (size_t col = 0; col < matrix[row].size(); col++) {
+        if (rows[row] || cols[col]) {
+          matrix[row][col] = 0;
+        }
+      }
+    }
+  }
+};
