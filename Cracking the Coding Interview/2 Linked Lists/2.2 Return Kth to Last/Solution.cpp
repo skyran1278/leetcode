9c95319f020014c685a7a544d95526596e2c4243
skyran1278 <skyran1278@gmail.com>
2025-11-10 07:09:40 +0000

    feat(linked-lists): add kth to last node finder
    - implement efficient algorithm to return the k-th to last node value
    - supports standard singly linked list traversal in $O(n)$ time and $O(1)$ space

---
+++ Cracking the Coding Interview/2 Linked Lists/2.2 Return Kth to Last/Solution.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+/**
+ * @brief 3 m 36 s
+ * O(n)
+ * O(1)
+ */
+class Solution {
+ public:
+  // 1->2->3->4->5 和 k = 2
+  int kthToLast(ListNode* head, int k) {
+    ListNode* kNode = head;
+    ListNode* tail = head;
+
+    for (size_t i = 0; i < k; i++) {
+      tail = tail->next;  // 3->4->5
+    }
+
+    while (tail != nullptr) {
+      kNode = kNode->next;  // 4->5
+      tail = tail->next;    //
+    }
+
+    return kNode->val;
+  }
+};
