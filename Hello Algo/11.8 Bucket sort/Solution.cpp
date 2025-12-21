097c0c4915eeea8fa3b7cf8eea03c161ea5f237c
skyran1278 <skyran1278@gmail.com>
2025-12-21 23:25:58 +0000

    feat(bucket-sort): implement bucket sort algorithm
    - adds a new bucket sort implementation to sort an array of integers
    - utilizes vector of vectors to create buckets based on range
    - sorts individual buckets before merging them back into the original array

---
+++ Hello Algo/11.8 Bucket sort/Solution.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 12 m 54 s
+ * O(n + k)
+ * O(n + k)
+ * depends on whether the sorting algorithm used within each bucket is stable.
+ */
+class Solution {
+ public:
+  vector<int> sortArray(vector<int>& nums) {
+    int smallest = *min_element(nums.begin(), nums.end());
+    int largest = *max_element(nums.begin(), nums.end());
+    size_t bucketCount = (largest - smallest) / 10 + 1;
+    vector<vector<int>> buckets(bucketCount);
+
+    for (auto&& num : nums) {
+      size_t i = (num - smallest) / 10;
+      buckets[i].push_back(num);
+    }
+
+    for (auto&& bucket : buckets) {
+      sort(bucket.begin(), bucket.end());
+    }
+
+    size_t i = 0;
+    for (auto&& bucket : buckets) {
+      for (auto&& num : bucket) {
+        nums[i++] = num;
+      }
+    }
+
+    return nums;
+  }
+};
