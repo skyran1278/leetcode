7cc32a663414ae5df219bf3535aa780619d32bf3
skyran1278 <skyran1278@gmail.com>
2026-01-08 17:32:12 +0000

    feat(ctci): add bit exchange solution
    - implement an efficient algorithm to swap odd and even bits
    - use bitmasking to perform the operation in constant time

---
+++ Cracking the Coding Interview/5 Bit Manipulation/5.7 Exchange/Solution2.cpp
@@ -0,0 +1,26 @@
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 9 m 32 s
+ * O(1)
+ * O(1)
+ */
+class Solution {
+ public:
+  int exchangeBits(int num) {
+    return (num >> 1 & 0x55555555) | (num << 1 & 0xaaaaaaaa);
+  }
+};
+
+int main() {
+  Solution s;
+  s.exchangeBits(1);
+}
