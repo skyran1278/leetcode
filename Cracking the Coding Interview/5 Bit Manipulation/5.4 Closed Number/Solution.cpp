90ff86bff776311a24886b2b724f5f4fe6f44475
skyran1278 <skyran1278@gmail.com>
2026-01-15 18:14:44 +0000

    refactor: simplify closed number bit manipulation logic
    - rewrite bit swapping and rearrangement logic to be more concise
    - update variable naming to clarify next larger and smaller values
    - improve readability of bitwise operations and loop conditions

--- Cracking the Coding Interview/5 Bit Manipulation/5.4 Closed Number/Solution.cpp
+++ Cracking the Coding Interview/5 Bit Manipulation/5.4 Closed Number/Solution.cpp
@@ -13,6 +13,7 @@ using namespace std;
  * @brief 20 m 46 s
  * @brief 38 m 54 s
  * @brief 50 m 34 s
+ * @brief 23 m 19 s
  * feel hard, need more time to understand it
  * O(1)
  * O(1)
@@ -20,77 +21,51 @@ using namespace std;
 class Solution {
  public:
   vector<int> findClosedNumbers(int num) {
-    int big = -1;
-    for (size_t i = 0; i < 31; i++) {
-      bool currentIs1 = num & 1 << i;
-      bool nextIs0 = !(num & 1 << i + 1);
-
-      if (currentIs1 && nextIs0) {
-        big = num;
-        big &= ~(1 << i);
-        big |= 1 << i + 1;
-
-        int left = 0;
-        int right = i - 1;
-        while (left < right) {
-          while (left < right && num & 1 << left) {
-            left++;
+    // 01 -> 10
+    int smallest = -1;
+    for (int i = 0; i < 30; i++) {
+      bool cur = num & 1 << i;
+      bool next = num & 1 << i + 1;
+      if (cur && !next) {
+        smallest = (num & ~(1 << i)) | 1 << i + 1;
+        int high = i - 1;
+        int low = 0;
+        while (high > low) {
+          while (high > low && !(num & 1 << high)) high--;
+          while (high > low && num & 1 << low) low++;
+          if (high > low) {
+            smallest = (smallest & ~(1 << high)) | 1 << low;
+            high--;
+            low++;
           }
-          while (left < right && !(num & 1 << right)) {
-            right--;
-          }
-
-          if (left < right) {
-            big |= 1 << left;
-            big &= ~(1 << right);
-          }
-
-          left++;
-          right--;
         }
-
         break;
       }
     }
 
-    int small = -1;
-    for (size_t i = 0; i < 31; i++) {
-      bool currentIs0 = !(num & 1 << i);
-      bool nextIs1 = num & 1 << i + 1;
-
-      if (currentIs0 && nextIs1) {
-        small = num;
-        small |= 1 << i;
-        small &= ~(1 << i + 1);
-
-        int left = 0;
-        int right = i - 1;
-        while (left < right) {
-          while (left < right && !(num & 1 << left)) {
-            left++;
+    // 10 -> 01
+    int largest = -1;
+    for (int i = 0; i < 30; i++) {
+      bool cur = num & 1 << i;
+      bool next = num & 1 << i + 1;
+      if (!cur && next) {
+        largest = (num & ~(1 << i + 1)) | 1 << i;
+        int high = i - 1;
+        int low = 0;
+        while (high > low) {
+          while (high > low && num & 1 << high) high--;
+          while (high > low && !(num & 1 << low)) low++;
+          if (high > low) {
+            largest = (largest & ~(1 << low)) | 1 << high;
+            high--;
+            low++;
           }
-          while (left < right && num & 1 << right) {
-            right--;
-          }
-
-          if (left < right) {
-            small &= ~(1 << left);
-            small |= 1 << right;
-          }
-
-          left++;
-          right--;
         }
-
         break;
       }
     }
 
-    if (num == __INT_MAX__) {
-      big = -1;
-    }
-
-    return {big, small};
+    return {smallest, largest};
   }
 };
 
