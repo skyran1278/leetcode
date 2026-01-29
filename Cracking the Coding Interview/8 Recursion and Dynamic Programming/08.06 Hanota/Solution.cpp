6726f04cf8a5a4fbfabafd64cb9222b38f218696
skyran1278 <skyran1278@gmail.com>
2026-01-29 14:45:03 +0000

    feat(hanota): implement towers of hanoi algorithm
    - provide a recursive solution for the towers of hanoi problem
    - use a depth-first search strategy to transfer disks between pegs
    - include time and space complexity analysis in the implementation

---
+++ Cracking the Coding Interview/8 Recursion and Dynamic Programming/08.06 Hanota/Solution.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief
+ * O(2^n)
+ * O(n)
+ */
+class Solution {
+ public:
+  void hanota(vector<int>& A, vector<int>& B, vector<int>& C) {
+    dfs(A.size(), A, B, C);
+  }
+
+  void dfs(size_t n, vector<int>& A, vector<int>& B, vector<int>& C) {
+    if (n == 1) {
+      move(A, C);
+      return;
+    }
+
+    dfs(n - 1, A, C, B);
+    move(A, C);
+    dfs(n - 1, B, A, C);
+  }
+
+  void move(vector<int>& A, vector<int>& B) {
+    B.push_back(A.back());
+    A.pop_back();
+  }
+};
