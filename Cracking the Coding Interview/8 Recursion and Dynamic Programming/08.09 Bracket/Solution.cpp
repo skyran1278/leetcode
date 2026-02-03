5709bd3ec408380576b8d2f86d61124c3a82e4ba
skyran1278 <skyran1278@gmail.com>
2026-02-03 01:27:24 +0000

    feat(recursion): add algorithm for generating valid parentheses
    - implement recursive backtracking to generate all valid combinations of n pairs of parentheses
    - enables solving combinatorial problems involving balanced brackets efficiently

---
+++ Cracking the Coding Interview/8 Recursion and Dynamic Programming/08.09 Bracket/Solution.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief
+ * O(n * Cn) where Cn is the n-th Catalan number
+ * O(n)
+ */
+class Solution {
+ public:
+  vector<string> generateParenthesis(int n) {
+    vector<string> result;
+    string state;
+    backtrack(result, state, n * 2, n, n);
+    return result;
+  }
+
+  void backtrack(vector<string>& result, string& state, int n, int left,
+                 int right) {
+    if (state.size() == n) {
+      result.push_back(state);
+      return;
+    }
+
+    if (left > 0) {
+      state.push_back('(');
+      backtrack(result, state, n, left - 1, right);
+      state.pop_back();
+    }
+
+    if (right > left) {
+      state.push_back(')');
+      backtrack(result, state, n, left, right - 1);
+      state.pop_back();
+    }
+  }
+};
