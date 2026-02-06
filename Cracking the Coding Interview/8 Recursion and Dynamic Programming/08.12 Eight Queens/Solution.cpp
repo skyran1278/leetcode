926c67288de03c53e5e5bff0182d59f0c8872491
skyran1278 <skyran1278@gmail.com>
2026-02-06 10:52:50 +0000

    feat(recursion): add n-queens backtracking solution
    - implement efficient backtracking algorithm to solve n-queens problem
    - enable generation of all valid board configurations for given size
    - support constraint checking for columns and diagonals for optimal performance

---
+++ Cracking the Coding Interview/8 Recursion and Dynamic Programming/08.12 Eight Queens/Solution.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief
+ * O(n!)
+ * O(n)
+ */
+class Solution {
+ public:
+  vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> result;
+    vector<string> state;
+    int row = 0;
+    vector<bool> usedCol(n);
+    vector<bool> usedDiag1(2 * n);
+    vector<bool> usedDiag2(2 * n);
+
+    backtrack(result, n, state, row, usedCol, usedDiag1, usedDiag2);
+
+    return result;
+  }
+
+  void backtrack(vector<vector<string>>& result, int n, vector<string>& state,
+                 int row, vector<bool>& usedCol, vector<bool>& usedDiag1,
+                 vector<bool>& usedDiag2) {
+    if (row == n) {
+      result.push_back(state);
+      return;
+    }
+
+    for (int col = 0; col < n; col++) {
+      int diag1 = row - col + (n - 1);
+      int diag2 = row + col;
+
+      if (usedCol[col] || usedDiag1[diag1] || usedDiag2[diag2]) continue;
+
+      usedCol[col] = usedDiag1[diag1] = usedDiag2[diag2] = true;
+      string rowString = string(n, '.');
+      rowString[col] = 'Q';
+      state.push_back(rowString);
+
+      backtrack(result, n, state, row + 1, usedCol, usedDiag1, usedDiag2);
+
+      usedCol[col] = usedDiag1[diag1] = usedDiag2[diag2] = false;
+      state.pop_back();
+    }
+  }
+};
