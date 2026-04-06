72628ed4148d11e23ec4c71c2fd1023a82492c94
skyran1278 <skyran1278@gmail.com>
2026-04-06 14:46:01 +0800

    refactor(leetcode): use array<int,2> for dp and add main in Stock with Fee

--- leetcode/714. Best Time to Buy and Sell Stock with Transaction Fee/Solution2.cpp
+++ leetcode/714. Best Time to Buy and Sell Stock with Transaction Fee/Solution2.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <climits>
 #include <list>
 #include <queue>
@@ -15,6 +16,7 @@ using namespace std;
  * @brief 4m 9s
  * @brief 4m 4s
  * @brief 2m 57s
+ * @brief 7m 50s
  *
  * Define dp[i, 0] as the maximum profit up to day i without holding stock,
  * and dp[i, 1] as maximum profit up to day i holding stock.
@@ -27,27 +29,25 @@ using namespace std;
  */
 class Solution {
  public:
-  // prices = [1,3,2,8,4,9], fee = 2
   int maxProfit(vector<int>& prices, int fee) {
-    vector<vector<int>> dp(prices.size(), vector<int>(2));
+    int n = static_cast<int>(prices.size());
+    vector<array<int, 2>> dp(n, {0, 0});
+    dp[0] = {0, -prices[0]};
 
-    dp[0][0] = 0;
-    dp[0][1] = -prices[0];
-
-    /**
-     * dp
-     * 1 0 -1
-     * 3 0 -1
-     * 2 0 -1
-     * 8 5 -1
-     * 4 5 1
-     * 9 8 1
-     */
-    for (size_t i = 1; i < prices.size(); i++) {
-      dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
-      dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+    for (int i = 1; i < n; ++i) {
+      dp[i][0] = max(dp[i - 1][0], prices[i] + dp[i - 1][1] - fee);
+      dp[i][1] = max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
     }
 
-    return dp[prices.size() - 1][0];
+    return dp[n - 1][0];
   }
 };
+
+int main() {
+  Solution s;
+  vector<int> prices{1, 3, 2, 8, 4, 9};
+  int fee = 2;
+  s.maxProfit(prices, fee);
+
+  return 0;
+}
