0cb0fc9b4004db1ca5ff00de002b14a6363e4104
skyran1278 <skyran1278@gmail.com>
2026-04-19 01:27:41 +0000

    refactor(leetcode): simplify Ugly Number II solution and add timing note
    - Move dp[0] initialization next to declaration
    - Collapse multi-line if blocks into single-line statements
    - Add recall timing note

--- leetcode/264. Ugly Number II/Solution.cpp
+++ leetcode/264. Ugly Number II/Solution.cpp
@@ -6,6 +6,7 @@ using namespace std;
 /**
  * @brief 13m 58s
  * @brief 8m 50s
+ * @brief 15m 35s
  * O(n)
  * O(n)
  */
@@ -13,12 +14,11 @@ class Solution {
  public:
   int nthUglyNumber(int n) {
     vector<int> dp(n);
+    dp[0] = 1;
 
     size_t index2 = 0;
     size_t index3 = 0;
     size_t index5 = 0;
-
-    dp[0] = 1;
     for (size_t i = 1; i < n; i++) {
       int next2 = dp[index2] * 2;
       int next3 = dp[index3] * 3;
@@ -26,15 +26,9 @@ class Solution {
 
       dp[i] = min({next2, next3, next5});
 
-      if (dp[i] == next2) {
-        index2++;
-      }
-      if (dp[i] == next3) {
-        index3++;
-      }
-      if (dp[i] == next5) {
-        index5++;
-      }
+      if (dp[i] == next2) index2++;
+      if (dp[i] == next3) index3++;
+      if (dp[i] == next5) index5++;
     }
 
     return dp[n - 1];
