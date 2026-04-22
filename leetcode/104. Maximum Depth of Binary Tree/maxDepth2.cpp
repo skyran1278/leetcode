e2d76bb6dcb4ed729f55456523b545b09ebaac95
skyran1278 <skyran1278@gmail.com>
2026-04-22 02:51:55 +0000

    style(leetcode): adjust pointer style in Maximum Depth of Binary Tree
    - Change `TreeNode *` to `TreeNode*` for consistent pointer placement
    - Add recall timing note

--- leetcode/104. Maximum Depth of Binary Tree/maxDepth2.cpp
+++ leetcode/104. Maximum Depth of Binary Tree/maxDepth2.cpp
@@ -17,23 +17,24 @@ using namespace std;
 
 struct TreeNode {
   int val;
-  TreeNode *left;
-  TreeNode *right;
+  TreeNode* left;
+  TreeNode* right;
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
+  TreeNode(int x, TreeNode* left, TreeNode* right)
       : val(x), left(left), right(right) {}
 };
 
 /**
  * @brief 4m 14s
+ * @brief 1m 13s
  * O(n)
  * O(h)
  */
 class Solution {
  public:
   // root = [3,9,20,null,null,15,7]
-  int maxDepth(TreeNode *root) {
+  int maxDepth(TreeNode* root) {
     if (root == nullptr) {
       return 0;
     }
